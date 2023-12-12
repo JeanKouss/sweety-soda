cfbbf860a103c7689a406dab75232e890802da81
Ekoé Jean <70731505+JeanKouss@users.noreply.github.com>
2023-12-12 12:01:50 +0000

    switch method upgraded, global improvement :art: :recycle:

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,34 @@
+// Layers select
 let sodaNameLayer = document.querySelector('#soda-name-layer');
 let fruitPiecesLayer = document.querySelector('#fruit-pieces-layer');
+let sodaBottleLayer = document.querySelector('#soda-bottle-layer');
+
+// Buttons select
 let previousSodaButtonContainer = document.getElementById('previous-soda-button-container');
 let nextSodaButtonContainer = document.getElementById('next-soda-button-container');
 
+// Vars
+let sodaSideTree = {
+    'apple-side' : {
+        'previous' : 'apple-side',
+        'next' : 'lemon-side',
+    },
+    'lemon-side' : {
+        'previous' : 'apple-side',
+        'next' : 'grappe-side',
+    },
+    'grappe-side' : {
+        'previous' : 'lemon-side',
+        'next' : 'cherry-side',
+    },
+    'cherry-side' : {
+        'previous' : 'grappe-side',
+        'next' : 'cherry-side',
+    }
+};
+let currentSodaSide = 'apple-side';
+
+// Functions
 function updateScrollPosition() {
     let sodaNameLayerHScrollRatio = sodaNameLayer.scrollLeft / sodaNameLayer.scrollWidth;
     // let fruitPiecesLayerVScrollRatio = fruitPiecesLayer.scrollTop / fruitPiecesLayer.scrollHeight
@@ -12,7 +38,18 @@ function updateScrollPosition() {
     // console.log('df');
 }
 
-requestAnimationFrame(updateScrollPosition);
+function updateSodaLayersSide(side) {
+    sodaNameLayer.classList.remove('apple-side', 'lemon-side', 'grappe-side', 'cherry-side');
+    fruitPiecesLayer.classList.remove('apple-side', 'lemon-side', 'grappe-side', 'cherry-side');
+    sodaBottleLayer.classList.remove('apple-side', 'lemon-side', 'grappe-side', 'cherry-side');
+    sodaNameLayer.classList.add(side);
+    fruitPiecesLayer.classList.add(side);
+    sodaBottleLayer.classList.add(side);
+    currentSodaSide = side;
+}
+
+// requestAnimationFrame(updateScrollPosition);
+
 
 function scrollToNextSoda() {
     sodaNameLayer.scrollBy({'top' : 0, 'left' : sodaNameLayer.scrollWidth/4, 'behavior' : 'smooth'})
@@ -23,9 +60,12 @@ function scrollToPreviousSoda() {
 }
 
 previousSodaButtonContainer.addEventListener('click', (e) => {
-    scrollToPreviousSoda();
+    // scrollToPreviousSoda();
+    updateSodaLayersSide(sodaSideTree[currentSodaSide]['previous']);
+
 });
 
 nextSodaButtonContainer.addEventListener('click', (e) => {
-    scrollToNextSoda();
+    // scrollToNextSoda();
+    updateSodaLayersSide(sodaSideTree[currentSodaSide]['next']);
 });
